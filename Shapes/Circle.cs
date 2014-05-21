fba32929a30ffcb14600c630cc63ced31eaff52b
Luca Scalzotto <lucascalzotto@gmail.com>
2014-05-21 15:35:44 +0200

    Added circle shape

---
+++ Shapes/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace ScorpionEngine.Shapes
+{
+    public class Circle
+    {
+        Point center;
+        int radius;
+
+        public Circle(Point center, int radius)
+        {
+            this.center = center;
+            this.radius = radius;
+        }
+
+        public Point Center
+        { get { return center; } set { center = value; } }
+        public int Radius
+        { get { return radius; } set { radius = value; } }
+
+        public int Diameter
+        { get { return radius * 2; } set { radius = value / 2; } }
+        public int Surface
+        { get { return (int)(radius * radius * Math.PI); } }
+        public int Circumference
+        { get { return (int)(2 * Math.PI * radius); } }
+    }
+}
