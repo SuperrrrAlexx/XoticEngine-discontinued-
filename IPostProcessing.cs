ba8c7df25f9615b35aab32234ebd7252866b4109
Luca Scalzotto <lucascalzotto@gmail.com>
2014-06-11 10:14:20 +0200

    IPostProcessing class

---
+++ IPostProcessing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XoticEngine
+{
+    public interface IPostProcessing
+    {
+        void Draw(Texture2D texture, SpriteBatch s, Vector2 pos);
+    }
+}
