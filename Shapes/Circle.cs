12fd2a635981164d9f0d4700c6d3afe50accab2c
Luca Scalzotto <lucascalzotto@gmail.com>
2014-07-04 17:19:29 +0200

    Shape intersections fix
    Circle and rectangle should only contain one point (not 2) for a line to
    intersect with it

--- Shapes/Circle.cs
+++ Shapes/Circle.cs
@@ -31,34 +31,19 @@ namespace XoticEngine.Shapes
 
         public bool Intersects(Circle circle)
         {
+            //Check if the distance is smaller than the two radiuses combined
             return Vector2.Distance(center.ToVector2(), circle.Center.ToVector2()) <= radius + circle.Radius;
         }
         public bool Intersects(Rectangle rect)
         {
+            //Get the smallest distance to a side
             Vector2 closest = new Vector2(MathHelper.Clamp(center.X, rect.Left, rect.Right), MathHelper.Clamp(center.Y, rect.Top, rect.Bottom));
 
             return (center.ToVector2() - closest).LengthSquared() < radius * radius;
         }
         public bool Intersects(Line line)
         {
-            //Check if the bounding boxes intersect
-            if (!boundingBox.Intersects(line.BoundingBox))
-                return false;
-
-            //Translate the space so P1 ends up at the origin:
-            Vector2 lineEnd = line.P2.ToVector2() - line.P1.ToVector2();
-            Vector2 circleCenter = center.ToVector2() - line.P1.ToVector2();
-
-            //Check if the line is a point
-            if (lineEnd.Length() == 0)
-                return circleCenter.LengthSquared() < this.radius * this.radius;
-
-            //Project circleCenter onto lineEnd:
-            //Calculate the lambda of the projection (of the form p1 + lambda * (p2 - p1)), and restrict it to [0, 1]
-            float lambda = MathHelper.Clamp(Vector2.Dot(lineEnd, circleCenter) / lineEnd.LengthSquared(), 0, 1);
-            Vector2 closestToLine = lambda * lineEnd - circleCenter;
-
-            return closestToLine.LengthSquared() < this.radius * this.radius;
+            return line.Intersects(this);
         }
 
         public Point Center
--- Shapes/Line.cs
+++ Shapes/Line.cs
@@ -36,14 +36,14 @@ namespace XoticEngine.Shapes
             float numerator2 = ((p1.Y - line.P1.Y) * (p2.X - p1.X)) - ((p1.X - line.P1.X) * (p2.Y - p1.Y));
 
             //Detect coincident lines
-            if (denominator == 0) return numerator1 == 0 && numerator2 == 0;
+            if (denominator == 0)
+                return numerator1 == 0 && numerator2 == 0;
 
             float r = numerator1 / denominator;
             float s = numerator2 / denominator;
 
             return (r >= 0 && r <= 1) && (s >= 0 && s <= 1);
         }
-
         public bool Intersects(Rectangle rectangle)
         {
             //Check if the bounding boxes intersect
@@ -51,7 +51,7 @@ namespace XoticEngine.Shapes
                 return false;
 
             //Check if the rectangle contains one of the points
-            if (rectangle.Contains(p1) || rectangle.Contains(p2))
+            if ((rectangle.Contains(p1) && !rectangle.Contains(p2)) || (rectangle.Contains(p2) && !rectangle.Contains(p1)))
                 return true;
 
             //Check if one of the rectangles lines intersects with the line
@@ -60,10 +60,30 @@ namespace XoticEngine.Shapes
                 || Intersects(new Line(new Point(rectangle.Bottom, rectangle.Right), new Point(rectangle.Bottom, rectangle.Left)))
                 || Intersects(new Line(new Point(rectangle.Bottom, rectangle.Left), new Point(rectangle.Top, rectangle.Left)));
         }
-
         public bool Intersects(Circle circle)
         {
-            return circle.Intersects(this);
+            //Check if the bounding boxes intersect
+            if (!boundingBox.Intersects(circle.BoundingBox))
+                return false;
+
+            //Check if the circle contains one of the points
+            if ((circle.Contains(p1) && !circle.Contains(p2)) || (circle.Contains(p2) && !circle.Contains(p1)))
+                return true;
+
+            //Translate the space so P1 ends up at the origin:
+            Vector2 lineEnd = p2.ToVector2() - p1.ToVector2();
+            Vector2 circleCenter = circle.Center.ToVector2() - p1.ToVector2();
+
+            //Check if the line is a point
+            if (lineEnd.Length() == 0)
+                return circleCenter.LengthSquared() < circle.Radius * circle.Radius;
+
+            //Project circleCenter onto lineEnd:
+            //Calculate the lambda of the projection (of the form p1 + lambda * (p2 - p1)), and restrict it to [0, 1]
+            float lambda = MathHelper.Clamp(Vector2.Dot(lineEnd, circleCenter) / lineEnd.LengthSquared(), 0, 1);
+            Vector2 closestToLine = lambda * lineEnd - circleCenter;
+
+            return closestToLine.LengthSquared() < circle.Radius * circle.Radius;
         }
 
         public Point P1
