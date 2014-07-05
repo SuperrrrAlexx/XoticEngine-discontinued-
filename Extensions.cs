21a877d55aeb1a63c8bf3e0485dc3f0af2865bae
Luca Scalzotto <lucascalzotto@gmail.com>
2014-07-05 12:14:33 +0200

    Removed useless code from DrawLine

--- Extensions.cs
+++ Extensions.cs
@@ -13,16 +13,11 @@ namespace XoticEngine
         //Draw a line
         public static void DrawLine(this SpriteBatch s, Line l, Color color)
         {
-            //Calculate the direction and angle
-            Vector2 p1 = new Vector2(l.P1.X, l.P1.Y);
-            Vector2 p2 = new Vector2(l.P2.X, l.P2.Y);
-            Vector2 direction = p2 - p1;
-            float angle = (float)Math.Atan2(direction.Y, direction.X);
-            //Calculate the distance
-            float length = Vector2.Distance(p1, p2);
+            //Calculate the direction
+            Vector2 direction = l.P2.ToVector2() - l.P1.ToVector2();
 
             //Draw a rotated rectangle with height 1 (a line)
-            s.Draw(Assets.Get<Texture2D>("DummyTexture"), new Rectangle((int)p1.X, (int)p1.Y, (int)length, 1), null, color, angle, Vector2.Zero, SpriteEffects.None, 0);
+            s.Draw(Assets.Get<Texture2D>("DummyTexture"), new Rectangle(l.P1.X, l.P1.Y, (int)direction.Length(), 1), null, color, direction.GetAngle(), Vector2.Zero, SpriteEffects.None, 0);
         }
 
         //Point and Vector2
