58af1f3a47d5075d8c5da2ca5c4017872ac226d1
Luca Scalzotto <lucascalzotto@gmail.com>
2014-07-15 10:15:40 +0200

    Added a background to label

--- GameObjects/MenuItems/Label.cs
+++ GameObjects/MenuItems/Label.cs
@@ -9,31 +9,91 @@ namespace XoticEngine.GameObjects.MenuItems
 {
     public class Label : GameObject
     {
+        //Text
         string text;
         SpriteFont font;
-        Color textColor;
+        Vector2 textPos;
+        //Colors
+        Color textColor, backColor;
+        //Background
+        Rectangle backRect;
+        Texture2D backTex;
 
-        public Label(string name, Vector2 position, float depth, string text, SpriteFont font, Color textColor)
-            : base(name, position, 0, Vector2.Zero, depth)
+        public Label(string name, Rectangle backRect, float depth, string text, SpriteFont font, Color textColor, Color backColor)
+            : base(name, backRect.Location.ToVector2(), 0, Vector2.Zero, depth)
         {
+            this.backRect = backRect;
             this.text = text;
             this.font = font;
             this.textColor = textColor;
+            this.backColor = backColor;
+            this.backTex = Assets.DummyTexture;
+
+            CalculateTextPosition();
+        }
+
+        void CalculateTextPosition()
+        {
+            //Calculate the text position
+            textPos.X = backRect.Center.X - font.MeasureString(text).X / 2;
+            textPos.Y = backRect.Center.Y - font.MeasureString(text).Y / 2;
         }
 
         public override void Draw(SpriteBatch gameBatch, SpriteBatch additiveBatch, SpriteBatch guiBatch)
         {
+            //Draw the background
+            guiBatch.Draw(backTex, new Rectangle((int)Position.X, (int)Position.Y, backRect.Width, backRect.Height), null, backColor, 0, Vector2.Zero, SpriteEffects.None, MathHelper.Clamp(Depth + float.Epsilon, 0, 1));
             //Draw the text
-            guiBatch.DrawString(font, text, Position, textColor, 0, Vector2.Zero, 1, SpriteEffects.None, Depth);
+            guiBatch.DrawString(font, text, textPos, textColor, 0, Vector2.Zero, 1, SpriteEffects.None, Depth);
             
             base.Draw(gameBatch, additiveBatch, guiBatch);
         }
 
+        public new Vector2 RelativePosition
+        {
+            get { return base.RelativePosition; }
+            set
+            {
+                base.RelativePosition = value;
+                backRect.Location = Position.ToPoint();
+                CalculateTextPosition();
+            }
+        }
+        //Text
         public string Text
-        { get { return text; } set { text = value; } }
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                CalculateTextPosition();
+            }
+        }
         public SpriteFont Font
-        { get { return font; } set { font = value; } }
+        {
+            get { return font; }
+            set
+            {
+                font = value;
+                CalculateTextPosition();
+            }
+        }
+        //Colors
         public Color TextColor
         { get { return textColor; } set { textColor = value; } }
+        public Color BackColor
+        { get { return backColor; } set { backColor = value; } }
+        //Background
+        public Rectangle BackRectangle
+        {
+            get { return backRect; }
+            set
+            {
+                backRect = value;
+                CalculateTextPosition();
+            }
+        }
+        public Texture2D BackTexture
+        { get { return backTex; } set { backTex = value; } }
     }
 }
