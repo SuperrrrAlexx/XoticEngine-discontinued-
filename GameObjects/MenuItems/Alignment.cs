f1152b9873342608013683e09455c3f3fd7b96a4
Luca Scalzotto <lucascalzotto@gmail.com>
2014-07-26 11:40:18 +0200

    Added Alignment struct
    Not used yet

---
+++ GameObjects/MenuItems/Alignment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XoticEngine.GameObjects.MenuItems
+{
+    public struct Alignment
+    {
+        private HorizontalAlignment horizontal;
+        private VerticalAlignment vertical;
+
+        public Alignment(HorizontalAlignment horizontal, VerticalAlignment vertical)
+        {
+            this.horizontal = horizontal;
+            this.vertical = vertical;
+        }
+
+        public HorizontalAlignment Horizontal
+        { get { return horizontal; } set { horizontal = value; } }
+        public VerticalAlignment Vertical
+        { get { return vertical; } set { vertical = value; } }
+    }
+
+    public enum HorizontalAlignment
+    { Left, Center, Right }
+    public enum VerticalAlignment
+    { Top, Center, Bottom }
+}
