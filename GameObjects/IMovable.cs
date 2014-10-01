8e38d19e5b6671a4af7d0d6ee612cd0562e3ab87
Luca Scalzotto <lucascalzotto@gmail.com>
2014-10-01 14:55:20 +0200

    Added IMovable and MoveOnInput

---
+++ GameObjects/IMovable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace XoticEngine.GameObjects
+{
+    public interface IMovable
+    {
+        void Move(Vector2 amount);
+    }
+}
