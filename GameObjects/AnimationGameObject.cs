ff2d24f91735a5ddb072cb7f95da237687d1fb76
Luca Scalzotto <lucascalzotto@gmail.com>
2014-10-02 10:42:35 +0200

    Re-added SpriteGameObject

--- GameObjects/AnimationGameObject.cs
+++ GameObjects/AnimationGameObject.cs
@@ -7,59 +7,41 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace XoticEngine.GameObjects
 {
-    public class AnimationGameObject : GameObject, IXDrawable
+    public class AnimationGameObject : SpriteGameObject
     {
         private Animation animation;
-        private Texture2D sprite;
-        private Color color = Color.White;
-        private SpriteEffects effects = SpriteEffects.None;
-        private DrawModes drawMode = DrawModes.AlphaBlend;
-        private Rectangle? sourceRect = null;
 
         public AnimationGameObject(string name, Animation animation, Vector2 position)
-            : base(name, position)
+            : base(name, animation.CurrentFrame, position)
         {
             this.animation = animation;
         }
         public AnimationGameObject(string name, Animation animation, Vector2 position, Color color)
-            : base(name, position)
+            : base(name, animation.CurrentFrame, position, color)
         {
             this.animation = animation;
-            this.color = color;
         }
         public AnimationGameObject(string name, Animation animation, Vector2 position, Color color, float rotation, Vector2 origin, float depth)
-            : base(name, position, rotation, origin, depth)
+            : base(name, animation.CurrentFrame, position, color, rotation, origin, depth)
         {
             this.animation = animation;
-            this.color = color;
         }
         public AnimationGameObject(string name, Animation animation, Vector2 position, Color color, float rotation, Vector2 origin, Vector2 scale, float depth)
-            : base(name, position, rotation, origin, scale, depth)
+            : base(name, animation.CurrentFrame, position, color, rotation, origin, scale, depth)
         {
             this.animation = animation;
-            this.color = color;
         }
 
         public override void Update()
         {
             //Update the animation and sprite
             animation.Update();
-            sprite = animation.CurrentFrame;
+            Sprite = animation.CurrentFrame;
 
             base.Update();
         }
 
         public Animation Animation
         { get { return animation; } set { animation = value; } }
-        public Texture2D Sprite
-        { get { return sprite; } set { sprite = value; } }
-        public Color DrawColor
-        { get { return color; } set { color = value; } }
-        public SpriteEffects Effects
-        { get { return effects; } set { effects = value; } }
-        public DrawModes DrawMode
-        { get { return drawMode; } set { drawMode = value; } }
-        public Rectangle? SourceRectangle
-        { get { return sourceRect; } set { sourceRect = value; } }
     }
 }
---
+++ GameObjects/SpriteGameObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XoticEngine.GameObjects
+{
+    public class SpriteGameObject : GameObject, IXDrawable
+    {
+        private DrawModes drawMode = DrawModes.AlphaBlend;
+        private Texture2D texture;
+        private Color color = Color.White;
+        private Rectangle? sourceRect = null;
+        private SpriteEffects effects = SpriteEffects.None;
+
+        public SpriteGameObject(string name, Texture2D texture, Vector2 position)
+            : base(name, position)
+        {
+            this.texture = texture;
+        }
+        public SpriteGameObject(string name, Texture2D texture, Vector2 position, Color color)
+            : base(name, position)
+        {
+            this.texture = texture;
+            this.color = color;
+        }
+        public SpriteGameObject(string name, Texture2D texture, Vector2 position, Color color, float rotation, Vector2 origin, float depth)
+            : base(name, position, rotation, origin, depth)
+        {
+            this.texture = texture;
+            this.color = color;
+        }
+        public SpriteGameObject(string name, Texture2D texture, Vector2 position, Color color, float rotation, Vector2 origin, Vector2 scale, float depth)
+            : base(name, position, rotation, origin, scale, depth)
+        {
+            this.texture = texture;
+            this.color = color;
+        }
+
+        public DrawModes DrawMode
+        { get { return drawMode; } set { drawMode = value; } }
+        public Texture2D Sprite
+        { get { return texture; } set { texture = value; } }
+        public Color DrawColor
+        { get { return color; } set { color = value; } }
+        public Rectangle? SourceRectangle
+        { get { return sourceRect; } set { sourceRect = value; } }
+        public SpriteEffects Effects
+        { get { return effects; } set { effects = value; } }
+    }
+}
